0ce3f74f2dee7143e9cde476d2ba4333353ca156
Pete Restall <pete@restall.net>
2025-03-22 01:44:12 +0000

    Added a failing integration test for exercising the end-to-end 'GET /companies/{id]' endpoint.

---
+++ src/EvaluationApi.Tests/Integration/GetSampleCompaniesByIdEndToEndTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using MiddlewareNz.EvaluationApi.Companies;
+using Xunit;
+
+namespace MiddlewareNz.EvaluationApi.Tests.Integration;
+
+[Collection(IntegrationTests.Collection)]
+public class GetSampleCompaniesByIdEndToEndTest : IClassFixture<WebAppFixture>
+{
+	private class Samples : TheoryData<int, Company>
+	{
+		public Samples()
+		{
+			this.Add(1, new Company(id: 1, name: "MWNZ", description: "..is awesome"));
+			this.Add(2, new Company(id: 2, name: "Other", description: "....is not"));
+		}
+	}
+
+	private readonly WebAppFixture fixture;
+
+	public GetSampleCompaniesByIdEndToEndTest(WebAppFixture fixture)
+	{
+		this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+	}
+
+	[Theory]
+	[ClassData(typeof(Samples))]
+	public async Task ExpectSampleCompanyCanBeRetrievedSuccessfully(int id, Company sample)
+	{
+		var restClient = this.fixture.RestClientFor<IGetCompanyByIdApi>();
+		var company = await restClient.GetCompanyById(id);
+		company.Should().BeEquivalentTo(sample);
+	}
+}
---
+++ src/EvaluationApi.Tests/Integration/IGetCompanyByIdApi.cs
@@ -0,0 +1,10 @@
+﻿using MiddlewareNz.EvaluationApi.Companies;
+using RestEase;
+
+namespace MiddlewareNz.EvaluationApi.Tests.Integration;
+
+public interface IGetCompanyByIdApi
+{
+    [Get("companies/{id}")]
+    Task<Company> GetCompanyById([Path("id")] int id);
+}
