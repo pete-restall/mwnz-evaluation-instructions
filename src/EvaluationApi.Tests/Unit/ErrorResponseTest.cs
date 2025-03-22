ca9b59a234757da25f2d9674a1b438d21b508953
Pete Restall <pete@restall.net>
2025-03-22 03:38:55 +0000

    Added the ErrorResponse DTO.

---
+++ src/EvaluationApi.Tests/Unit/ErrorResponseTest.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using Lophtware.Testing.Utilities.NonDeterminism.PrimitiveGeneration;
+using Xunit;
+
+namespace MiddlewareNz.EvaluationApi.Tests.Unit;
+
+public class ErrorResponseTest
+{
+	[Fact]
+	public void Constructor_CalledWithNullError_ExpectArgumentNullExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateErrorResponseWithError(null!);
+		constructor
+			.Should().Throw<ArgumentNullException>()
+			.WithParameterName("error");
+	}
+
+	private static ErrorResponse CreateErrorResponseWithError(string error) => new(error, AnyErrorDescription());
+
+	private static string AnyErrorDescription() => StringGenerator.AnyNonNullNonWhitespaceNonEmpty();
+
+	[Fact]
+	public void Constructor_CalledWithEmptyError_ExpectArgumentExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateErrorResponseWithError("");
+		constructor
+			.Should().Throw<ArgumentException>()
+			.WithParameterName("error");
+	}
+
+	[Fact]
+	public void Constructor_CalledWithWhitespaceError_ExpectArgumentExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateErrorResponseWithError(StringGenerator.AnyWhitespace());
+		constructor
+			.Should().Throw<ArgumentException>()
+			.WithParameterName("error");
+	}
+
+	[Fact]
+	public void Error_Get_ExpectTrimmedValuePassedToConstructor()
+	{
+		var trimmedError = AnyError().Trim();
+		var response = CreateErrorResponseWithError(trimmedError.WrapInWhitespace());
+		response.Error.Should().Be(trimmedError);
+	}
+
+	private static string AnyError() => StringGenerator.AnyNonNullNonWhitespaceNonEmpty();
+
+	[Fact]
+	public void Constructor_CalledWithNullErrorDescription_ExpectArgumentNullExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateErrorResponseWithErrorDescription(null!);
+		constructor
+			.Should().Throw<ArgumentNullException>()
+			.WithParameterName("errorDescription");
+	}
+
+	private static ErrorResponse CreateErrorResponseWithErrorDescription(string description) => new(AnyError(), description);
+
+	[Fact]
+	public void Constructor_CalledWithEmptyErrorDescription_ExpectArgumentExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateErrorResponseWithErrorDescription("");
+		constructor
+			.Should().Throw<ArgumentException>()
+			.WithParameterName("errorDescription");
+	}
+
+	[Fact]
+	public void Constructor_CalledWithWhitespaceErrorDescription_ExpectArgumentExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateErrorResponseWithErrorDescription(StringGenerator.AnyWhitespace());
+		constructor
+			.Should().Throw<ArgumentException>()
+			.WithParameterName("errorDescription");
+	}
+
+	[Fact]
+	public void ErrorDescription_Get_ExpectTrimmedValuePassedToConstructor()
+	{
+		var trimmedErrorDescription = AnyErrorDescription().Trim();
+		var response = CreateErrorResponseWithErrorDescription(trimmedErrorDescription.WrapInWhitespace());
+		response.ErrorDescription.Should().Be(trimmedErrorDescription);
+	}
+}
---
+++ src/EvaluationApi/ErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace MiddlewareNz.EvaluationApi;
+
+public class ErrorResponse
+{
+	public ErrorResponse(string error, string errorDescription)
+	{
+		this.Error = error?.Trim() ?? throw new ArgumentNullException(nameof(error));
+		if (this.Error == "")
+			throw new ArgumentException("Error must be specified", nameof(error));
+
+		this.ErrorDescription = errorDescription?.Trim() ?? throw new ArgumentNullException(nameof(errorDescription));
+		if (this.ErrorDescription == "")
+			throw new ArgumentException("Error Description must be specified", nameof(errorDescription));
+	}
+
+	public string Error { get; }
+
+	public string ErrorDescription { get; }
+}
