a189c37fbf244dfc3046b9a37cbd4bda9c38e93e
Pete Restall <pete@restall.net>
2025-03-22 01:42:54 +0000

    Added a Company DTO.

---
+++ src/EvaluationApi.Tests/Unit/CompanyTest.cs
@@ -0,0 +1,113 @@
+﻿using FluentAssertions;
+using Lophtware.Testing.Utilities.NonDeterminism.PrimitiveGeneration;
+using MiddlewareNz.EvaluationApi.Companies;
+using Xunit;
+
+namespace MiddlewareNz.EvaluationApi.Tests.Unit;
+
+public class CompanyTest
+{
+	[Theory]
+	[InlineData(0)]
+	[InlineData(-1)]
+	[InlineData(-10)]
+	[InlineData(-12387)]
+	public void Constructor_CalledWithInvalidId_ExpectArgumentOutOfRangeExceptionWithCorrectParamNameAndInvalidValue(int invalidId)
+	{
+		var constructor = () => CreateCompanyWithId(invalidId);
+		constructor
+			.Should().Throw<ArgumentOutOfRangeException>()
+			.WithParameterName("id")
+			.And.ActualValue.Should().Be(invalidId);
+	}
+
+	private static Company CreateCompanyWithId(int id) => new(id, AnyName(), AnyDescription());
+
+	private static string AnyName() => StringGenerator.AnyNonNullNonWhitespaceNonEmpty();
+
+	private static string AnyDescription() => StringGenerator.AnyNonNullNonWhitespaceNonEmpty();
+
+	[Fact]
+	public void Id_Get_ExpectSameValuePassedToConstructor()
+	{
+		var id = AnyId();
+		var company = CreateCompanyWithId(id);
+		company.Id.Should().Be(id);
+	}
+
+	[Fact]
+	public void Constructor_CalledWithNullName_ExpectArgumentNullExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateCompanyWithName(null!);
+		constructor
+			.Should().Throw<ArgumentNullException>()
+			.WithParameterName("name");
+	}
+
+	private static Company CreateCompanyWithName(string name) => new(AnyId(), name, AnyDescription());
+
+	private static int AnyId() => IntegerGenerator.GreaterThan(0);
+
+	[Fact]
+	public void Constructor_CalledWithEmptyName_ExpectArgumentExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateCompanyWithName("");
+		constructor
+			.Should().Throw<ArgumentException>()
+			.WithParameterName("name");
+	}
+
+	[Fact]
+	public void Constructor_CalledWithWhitespaceName_ExpectArgumentExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateCompanyWithName(StringGenerator.AnyWhitespace());
+		constructor
+			.Should().Throw<ArgumentException>()
+			.WithParameterName("name");
+	}
+
+	[Fact]
+	public void Name_Get_ExpectTrimmedValuePassedToConstructor()
+	{
+		var trimmedName = AnyName().Trim();
+		var company = CreateCompanyWithName(trimmedName.WrapInWhitespace());
+		company.Name.Should().Be(trimmedName);
+	}
+
+	[Fact]
+	public void Constructor_CalledWithNullDescription_ExpectArgumentNullExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateCompanyWithDescription(null!);
+		constructor
+			.Should().Throw<ArgumentNullException>()
+			.WithParameterName("description");
+	}
+
+	private static Company CreateCompanyWithDescription(string description) => new(AnyId(), AnyName(), description);
+
+	[Fact]
+	public void Constructor_CalledWithEmptyDescription_ExpectArgumentExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateCompanyWithDescription("");
+		constructor
+			.Should().Throw<ArgumentException>()
+			.WithParameterName("description");
+	}
+
+	[Fact]
+	public void Constructor_CalledWithWhitespaceDescription_ExpectArgumentExceptionWithCorrectParamName()
+	{
+		var constructor = () => CreateCompanyWithDescription(StringGenerator.AnyWhitespace());
+		constructor
+			.Should().Throw<ArgumentException>()
+			.WithParameterName("description");
+	}
+
+	[Fact]
+	public void Description_Get_ExpectTrimmedValuePassedToConstructor()
+	{
+		var trimmedDescription = AnyDescription().Trim();
+		var company = CreateCompanyWithDescription(trimmedDescription.WrapInWhitespace());
+		company.Description.Should().Be(trimmedDescription);
+	}
+}
---
+++ src/EvaluationApi/Companies/Company.cs
@@ -0,0 +1,23 @@
+﻿namespace MiddlewareNz.EvaluationApi.Companies;
+
+public class Company
+{
+	public Company(int id, string name, string description)
+	{
+		this.Id = id > 0 ? id : throw new ArgumentOutOfRangeException(nameof(id), id, "Company ID must be a positive integer");
+
+		this.Name = name?.Trim() ?? throw new ArgumentNullException(nameof(name));
+		if (this.Name == "")
+			throw new ArgumentException("Company Name must be specified", nameof(name));
+
+		this.Description = description?.Trim() ?? throw new ArgumentNullException(nameof(description));
+		if (this.Description == "")
+			throw new ArgumentException("Company Description must be specified", nameof(description));
+	}
+
+	public int Id { get; }
+
+	public string Name { get; }
+
+	public string Description { get; }
+}
